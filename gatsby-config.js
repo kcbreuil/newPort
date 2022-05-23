4a59165240d1471aaaf707abdee34901a320c88a
Katie Breuil <kcbreuil@gmail.com>
2022-05-23 00:00:29 -0400

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `KB`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Katie B.`,
+    description: `Katie Breuil is a killer frontend engineer.`,
+    author: `@kayseabee`,
+    siteUrl: `https://katiebreuil.com/`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
@@ -29,7 +29,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     {
