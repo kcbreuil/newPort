542e33f5484e6961a223967b13b9b1a8d1103a5e
Katie Breuil <kcbreuil@gmail.com>
2022-05-24 23:04:20 -0400

    test spacing

--- src/components/header.js
+++ src/components/header.js
@@ -20,11 +20,11 @@ const navItems = [
 ]
 const Header = ({ siteTitle, isScrolled }) => (
   <header className={`${isScrolled ? `bg-brand-blue`	: `transparent`} w-full sticky top-0 z-50 py-3`}>
-    <nav className="flex flex-col md:flex-row m-4 md:justify-between items-center">
+    <nav className="flex flex-col md:flex-row m-4 md:justify-between justify-center items-center">
       <Link to="/">
         <StaticImage src="../images/KB_s_cherry.png" alt="logo" width={150}/>
       </Link>
-      <div className="mt-2 flex flex-row gap-4 text-brand-black">
+      <div className="mt-2 flex flex-row gap-2 md:gap-4 text-brand-black">
         {navItems.map(item => {
           return <AnchorLink className="hover:underline decoration-wavy decoration-brand-yellow text-3xl" to={item.link}>{item.name}</AnchorLink>
         })}
